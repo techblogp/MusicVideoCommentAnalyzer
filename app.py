a2cc9e05ed7b74befed7a1a3a2eaf1c6515378fa
techblogp <techblogp@gmail.com>
2020-10-30 16:13:37 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ import string as str
 import nltkModules
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import spacy
-import spacy spacy.load('en_core_web_sm')
+import spacy, spacy.load('en_core_web_sm')
 from nltk.corpus import stopwords
 # import en_core_web_sm
 # nlp = en_core_web_sm.load()
