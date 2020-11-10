e8346938656e90f6d976f677f05a73880d55657c
techblogp <techblogp@gmail.com>
2020-11-10 10:34:59 -0600

    Add files via upload

--- nltkModules.py
+++ nltkModules.py
@@ -11,7 +11,9 @@ nltk.download('wordnet')
 nltk.download('punkt')
 
 import spacy
-!pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.2.0/en_core_web_sm-2.2.0.tar.gz
-spacy.load('en_core_web_sm')
+# !pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.2.0/en_core_web_sm-2.2.0.tar.gz
+# spacy.load('en_core_web_sm')
 
+import pip
+failed = pip.main(["install", en_core_web_sm])
 
